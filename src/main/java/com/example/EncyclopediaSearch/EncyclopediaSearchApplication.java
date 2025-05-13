3f8d5a8985c84e9683eb11cac518ef746852cbde
ImjaeMVP <yesa1504@gmail.com>
2025-05-13 23:15:55 +0900

    init project

---
+++ src/main/java/com/example/EncyclopediaSearch/EncyclopediaSearchApplication.java
@@ -0,0 +1,13 @@
+package com.example.EncyclopediaSearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EncyclopediaSearchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EncyclopediaSearchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/EncyclopediaSearch/EncyclopediaSearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.EncyclopediaSearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EncyclopediaSearchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
